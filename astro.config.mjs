d9fef634d7bdd988a6333dad485e65c61ba7bdd6
Fran Cortez <francisco.cortez@savings-united.com>
2025-10-07 10:21:33 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: netlify(),
+  redirects: {
+    '/': '/retreats',
+  },
+});
