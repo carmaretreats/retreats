64e789aec311abcde736c8e107a7700c776b7833
Fran Cortez <francisco.cortez@savings-united.com>
2025-10-19 12:15:07 +0200

    Update testimonials carousel

--- public/scripts/slider.data.js
+++ public/scripts/slider.data.js
@@ -1,17 +1,27 @@
 window.testimonials = [
   {
-    name: "Lorem P. Ipsum",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    img: "https://images.unsplash.com/photo-1616435577207-ca90abc6b732?auto=format&fit=crop&w=400&q=80"
+    name: "Mona, Deutschland",
+    text: "Das Yoga Retreat mit Mareen und Carmen war einfach toll! Ich habe mich von Anfang bis Ende so wohl gefühlt und es war eine ganz wunderbare Atmosphäre. Eine perfekte Mischung aus Yoga, Zeit zum entspannen, tollen Gesprächen, Ausflügen, leckerem Essen….Wir haben auch gemeinsam einen Kochkurs gemacht, dabei gelernt die für Sri Lanka typische Currys zuzubereiten! Das Haus ist nah am Strand und in traumhafter Umgebung. Ich kenne Mareen schon aus ihren Berliner Yogaklassen – Mareen und Carmen unterrichten mit so viel Herz und Inspiration, dass man sich sofort wohlfühlt. Die beiden haben einen Ort geschaffen, an dem man wirklich abschalten kann. Danke liebe Mareen & Carmen für diese einzigartige Zeit bei euch im Carma Retreat, ich freu mich schon aufs nächste Mal!",
+    img: "/images/testimonial/mona.jpeg"
   },
   {
-    name: "Lorem De Ipsum",
-    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
-    img: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=400&q=80"
+    name: "Elena",
+    text: "Mein bestes Retreat bisher in Sri Lanka! Top organisiert und perfekter Ablauf, aber was soll auch passieren, wenn sich eine Deutsche und eine Schweizerin zusammentun :-)",
+    img: "/images/testimonial/elena.jpeg"
   },
   {
-    name: "Ms. Lorem R. Ipsum",
-    text: "Duis aute irure dolor in reprehenderit in voluptate velit esse.",
-    img: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80"
+    name: "Heike",
+    text: "Höchstes Niveau! Neben Yoga und dem „süßen Leben“ in Sri Lanka hat mich vor allem die Auswahl an Mit-Yogis begeistert: Was für tolle Leute. Hätte ich nicht gedacht, so viele Menschen zu treffen, die genau so ticken wie ich!",
+    img: "/images/testimonial/heike.jpeg"
+  },
+  {
+    name: "Svenja, Deutschland",
+    text: "Mareen schafft kreative Yoga Flows in die sie auch Pranayama und Meditation integriert. Es ist jedes Mal eine Freude von ihr präzise durch die Stunden geleitet zu werden, dabei komplett loszulassen und zugleich immer mehr Stärke und Flexibilität zu entwickeln.",
+    img: "/images/testimonial/mona.jpeg"
+  },
+  {
+    name: "Nele, Deutschland",
+    text: "Seit 2020 übe ich Yoga mit Mareen. Und ich liebe Ihre Yogastunden! Ihr Timing ist bemerkenswert, sie kreiert tolle Stunden, motiviert, bringt einem immer wieder was Neues bei und trägt dazu bei, dass mir Yoga so so gut tut. Werde immer Fan sein und froh darüber, dass ich Mareens Yoga entdeckt habe. Absolut empfehlenswert ❤️",
+    img: "/images/testimonial/elena.jpeg"
   }
 ];
