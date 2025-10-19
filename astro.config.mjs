33e86cfa449ee8633f481d8611c91a703c9ab7aa
Fran Cortez <francisco.cortez@savings-united.com>
2025-10-19 13:35:42 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import alpinejs from '@astrojs/alpinejs';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://www.carma-retreats.com/',
   output: 'server',
   adapter: netlify(),
   integrations: [
     alpinejs({ entrypoint: '/src/scripts/alpine.ts' }),
     react(),
+    sitemap(),
   ],
     vite: {
     plugins: [tailwindcss()]
