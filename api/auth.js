45730f1ee1f4f0beb1e66726be022a0b2ae8e7c2
Fran Cortez <francisco.cortez@savings-united.com>
2025-10-29 15:32:35 +0100

    Add decap config from docs

--- api/auth.js
+++
@@ -1,6 +0,0 @@
-export function GET(request, res) {
-  const CLIENT_ID = import.meta.env.GITHUB_CLIENT_ID;
-  const REDIRECT_URI = import.meta.env.REDIRECT_URI;
-  const authURL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=repo,user`;
-  return Response.redirect(authURL, 307);
-}
